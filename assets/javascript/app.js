87fc77606903ed6ae456992009ee36db6f5db385
FlyMoe <rrgarcia@gmail.com>
2016-05-06 16:11:15 -0400

    Buttons are now added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ var tvShows = ['Mash','The A-Team', 'Seinfeld', 'Game of Thrones', 'Walking Dead
 
  	// Generic function for displaying movie data 
 	function renderButtons(){ 
-		alert("dude");
+
 	    // Deletes the movies prior to adding new movies (this is necessary otherwise you will have repeat buttons)
 	    $('#buttons').empty();
 
@@ -16,7 +16,7 @@ var tvShows = ['Mash','The A-Team', 'Seinfeld', 'Game of Thrones', 'Walking Dead
 
 	      // Note the jQUery syntax here... 
 	        var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-	        a.addClass('movie'); // Added a class 
+	        a.addClass('myButtons'); // Added a class 
 	        a.attr('data-name', tvShows[i]); // Added a data-attribute
 	        a.text(tvShows[i]); // Provided the initial button text
 	        $('#buttons').append(a); // Added the button to the HTML
@@ -27,13 +27,23 @@ var tvShows = ['Mash','The A-Team', 'Seinfeld', 'Game of Thrones', 'Walking Dead
 	$('#addMovie').on('click', function(){
 
 		// This line of code will grab the input from the textbox
-		var movie = $('#movie-input').val().trim();
+		var tvShow = $('#movie-input').val().trim();
 
-		// The movie from the textbox is then added to our array
-		tvShows.push(movie);
+		if (tvShow === "" && typeof tvShow === "string") {
 
-		// Our array then runs which handles the processing of our movie array
-		renderButtons();
+			$("#errorMessage").show();
+			$("#errorMessage").html("You need to enter a TV show!");
+			
+		} else {
+
+			$("#errorMessage").hide();
+
+			// The movie from the textbox is then added to our array
+			tvShows.push(tvShow);
+
+			// Our array then runs which handles the processing of our movie array
+			renderButtons();
+		}
 
 		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
 		return false;
